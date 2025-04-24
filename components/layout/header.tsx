decff9bac4b00fde6e55806e9f3dc47bc0d243bd
ParkKyungWan <pkw8117197@gmail.com>
2025-04-24 17:13:02 +0900

    [feat] 헤더 드롭다운

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,14 +1,57 @@
 "use client";
 
-import { RECUITMENT_START_DATE, RECRUITMENT_END_DATE } from "@/constants/recruitment";
+import { RECUITMENT_START_DATE, RECRUITMENT_END_DATE, RECUITMENT_PARTS } from "@/constants/recruitment";
 import { MAX_LAYOUT_WIDTH } from "@/constants/layout";
+import { motion } from 'framer-motion';
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+const PartsDropDown: React.FC = () => {
+  const parentsVariants = {
+    hidden: {
+      height: 0,
+      opacity: 0,
+    },
+    show: {
+      height: 'auto',
+      opacity: 1,
+      transition: {
+        height: { duration: 0.2, ease: 'easeInOut' },
+        opacity: { duration: 0.2, ease: 'easeInOut' },
+        staggerChildren: 0.05, 
+      },
+    },
+  };
+
+  const childrenVariants = {
+    hidden: { opacity: 0, y: 8 },
+    show: { opacity: 1, y: 0, transition: { duration: 0.3, ease: 'easeOut' } },
+  }
+
+  return (
+    <div className="absolute z-[150] left-0 top-10.5 -translate-x-3">
+      <div id="dropDownList" className="w-full flex flex-col items-center justify-center mb-0.5 rounded-lg bg-[rgba(0,0,0,0.88)] backdrop-blur-md border shadow-[0_0_24px_rgba(255,255,255,0.08)] border-white/10 px-4 py-1.5 gap-3"> 
+        <button className="font-gradient">Codin 코딘</button>
+        <motion.div id="dropDownList" className="w-full flex flex-col items-center justify-center gap-3"
+          layout variants={parentsVariants} initial="hidden" animate="show"
+        > 
+          { RECUITMENT_PARTS.map((part, index)=>(
+            <motion.div key={index} variants={childrenVariants}>
+              <button className="text-sub hover:text-white font:font-bold">{part.simple}</button>
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
+    </div>
+  );
+}
 
 const Header: React.FC = () => {
   const router = useRouter();
   const handleLogoClick = () =>{
     router.push("/");
   }
+  const [dropDownHovered, setdropDownHovered] = useState<boolean>(false)
   
     return (
       <header className="fixed top-0 left-0 z-100 w-full flex justify-center backdrop-blur-md items-center px-4 sm:px-8 h-[68px] max-h-[68px]">
@@ -18,7 +61,14 @@ const Header: React.FC = () => {
             </button>
             <div id="BtnsArea" className="text-sm font-medium flex gap-2 sm:gap-4 text-[rgba(255,255,255,0.70)]">
                 <p id="모집기간" className="sm:flex items-center whitespace-nowrap hidden">{RECUITMENT_START_DATE} ~ {RECRUITMENT_END_DATE}</p>
-                <button id="모집분야" className="flex items-center whitespace-nowrap w-max"><span>모집 분야</span> <img src="/icons/listdown.svg" width={24}/></button>
+                <div id="모집분야" className="flex items-center whitespace-nowrap w-max relative"
+                  onMouseEnter={()=>setdropDownHovered(true)} onMouseLeave={()=>setdropDownHovered(false)}>
+                  <button className="flex items-center justify-center" onClick={()=>{setdropDownHovered((prev)=>!prev)}}> 
+                    모집 분야 
+                    <img src="/icons/listdown.svg" width={24}/> 
+                  </button>
+                  { dropDownHovered && <PartsDropDown/>} 
+                </div>
                 <button id="지원하기" className="flex items-center whitespace-nowrap rounded-lg sm:rounded-xl bg-[rgba(255,255,255,0.25)] py-2 px-4 sm:py-3 sm:px-10">바로 지원하기</button>
             </div>
         </div>
--- constants/recruitment.tsx
+++ constants/recruitment.tsx
@@ -1,2 +1,33 @@
 export const RECUITMENT_START_DATE = "25.04.00";
 export const RECRUITMENT_END_DATE = "25.05.00";
+
+export const RECUITMENT_PARTS = [
+    {
+      simple: "기획/PM",
+      titleEN: "Project Manager",
+      titleKR: "기획 및 총괄",
+      explain: "새로운 기능을 기획하고 팀을 운영해요",
+      iconPATH: "/icons/recruitParts/pmIcon.png",
+    },
+    {
+      simple: "디자이너",
+      titleEN: "UI/UX Designer",
+      titleKR: "디자이너",
+      explain: "UI/UX, 레이아웃, 아이콘 등을 디자인해요",
+      iconPATH: "/icons/recruitParts/designerIcon.png",
+    },
+    {
+      simple: "프론트엔드",
+      titleEN: "FE Developer",
+      titleKR: "프론트엔드 개발자",
+      explain: "디자인을 구현하고 인터랙션을 개발해요",
+      iconPATH: "/icons/recruitParts/developerIcon.png",
+    },
+    {
+      simple: "백엔드",
+      titleEN: "BE Developer",
+      titleKR: "백엔드 개발자",
+      explain: "데이터를 설계하고 서버 API를 구현해요",
+      iconPATH: "/icons/recruitParts/developerIcon.png",
+    },
+];
