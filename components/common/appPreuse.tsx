ec2bd430e489805cbd55e897c41f498f62c647f0
ParkKyungWan <pkw8117197@gmail.com>
2025-04-24 20:47:54 +0900

    [perf] appPreuse-> SSR변경

--- components/common/appPreuse.tsx
+++ components/common/appPreuse.tsx
@@ -1,12 +1,10 @@
-"use client"
-
 const AppPreuse: React.FC = () => {
    
     return (
         <div 
             className="relative w-[295.4px] h-[581px] flex items-center justify-center"
         >   
-            <iframe src="https://codin.inu.ac.kr" 
+            <iframe src="https://codin.inu.ac.kr/" 
                 sandbox="allow-scripts allow-same-origin allow-popups"
                 className="z-5 absolute left-[-25%] top-[-24%] w-[150%] h-[150%] scale-[0.57] origin-center">
             </iframe>
@@ -17,7 +15,7 @@ const AppPreuse: React.FC = () => {
             <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
                 <div className="bg-white w-[260.4px] h-[541px] rounded-md"/>
             </div>
-                
+            
         </div>
     );
 
